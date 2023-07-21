2f780f5e13fa445eecdd52a3580385b69d200c3d
annah2544 <hannahwanjuhi0@gmail.com>
2023-07-21 11:16:45 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,33 +4,18 @@
  * @n: number of times the character _ should be printed
  */
 void print_line(int n)
-
 {
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i;
 
-        if (n <= 0)
-
-        {
-
-                _putchar('\n');
-
-        } else
-
-        {
-
-                int i;
-
-
-                for (i = 1; i <= n; i++)
-
-                {
-
-                        _putchar('_');
-
-                }
-
-                _putchar('\n');
-
-        }
-
-
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
